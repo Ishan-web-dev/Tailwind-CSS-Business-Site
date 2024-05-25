d49d0205837de7fef9c609f753e58373e67689c9
ishan-web-dev <joshiishaan86@gmail.com>
2024-05-25 13:03:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const sidebar = document.getElementById('sidebar');
+const openSidebarButton = document.getElementById('open-sidebar');
+
+openSidebarButton.addEventListener('click', (e) => {
+    e.stopPropagation();
+    sidebar.classList.toggle('-translate-x-full');
+});
+
+// Close the sidebar when clicking outside of it
+document.addEventListener('click', (e) => {
+    if (!sidebar.contains(e.target) && !openSidebarButton.contains(e.target)) {
+        sidebar.classList.add('-translate-x-full');
+    }
+});
+
+     window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+      } else {
+        document.getElementById("navbar").style.backgroundColor = "transparent";
+      }
+    }
